f53f8c7f094e14b6ae96fa03e3dbae04b356ad61
LinyuLike <LinyuLike@outlook.com>
2025-04-23 11:10:53 +0800

    实现简单的reactor

---
+++ reactor.c
@@ -0,0 +1,188 @@
+
+
+#include <errno.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <poll.h>
+#include <sys/epoll.h>
+#include <errno.h>
+#include <sys/time.h>
+
+
+#define CONNECTION_SIZE 1048576
+#define MAX_PORTS       20
+
+#define HTTP_MODE   0
+#define WS_MODE     1
+
+// 前置声明
+int accept_cb(int fd);
+int recv_cb(int fd);
+int send_cb(int fd);
+
+// 全局变量
+int epfd = 0;
+struct conn conn_list[CONNECTION_SIZE] = {0};
+
+
+// 为一个fd在epoll中设置事件
+// flag非零为增加，否则为更新
+int set_event(int fd, int event, int flag){
+    if(flag){
+        struct epoll_event ev;
+        ev.events = event;
+        ev.data.fd = fd;
+        epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev);
+    } else {
+        struct epoll_event ev;
+        ev.events = event;
+        ev.data.fs = fd;
+        epoll_ctl(epfd, EPOLL_CTL_MOD, fd, &ev);
+    }
+}
+
+// 为一个client fd在epoll中注册event，并在数据数组中初始化
+int event_register_client(int fd, int event){
+    if(fd < 0) return -1;
+
+    conn_list[fd].fd = fd;
+    conn_list[fd].r_action.recv_callback = recv_cb;
+    conn_list[fd].send_callback = send_cb;
+
+    memset(connn_list[fd].rbuffer, 0, BUFFER_LENGTH);
+    conn_list[fd].rlength = 0;
+
+    memset(conn_list[fd].rbuffer, 0, BUFFER_LENGTH);
+    conn_list[fd].rlength = 0;
+
+    set_event(fd, event, 1);
+}
+
+
+int accept_cb(int fd){
+    struct sockaddr_in clientaddr;
+    socklen_t len = sizeof(clientaddr);
+
+    int clientfd = accept(fd, (struct sockaddr*)&clientaddr, &len);
+    if(clientfd < 0){
+        printf("accept errno: %d --> %s\n", errno, strerror(errno));
+		return -1;
+    }
+
+    event_register_client(clientfd, EPOLLIN);
+
+    return 0;
+}
+
+int recv_cb(int fd){
+    memset(conn_list[fd].rbuffer, 0, BUFFER_LENGTH);
+    // note recv的最后一个参数：
+    // 0 : 默认行为
+    // MSG_PEEK：查看数据但不从接收队列中移除
+    // MSG_WAITALL：等待直到接收到请求的字节数
+    // MSG_DONTWAIT：非阻塞操作，即使socket设置为阻塞模式
+    // MSG_OOB：处理带外数据
+    int count = recv(fd, conn_list[fd].rbuffer, BUFFER_LENGTH, 0);
+
+    // note recv的返回值:
+    // 如果某个socket fd触发了可读事件，但是recv函数读取到的长度为0，通常表明连接已经被对方正常关闭了。
+    // 因为在TCP协议中，对方调用close()时会发送一个FIN包，这会出发可读事件，
+    // 但实际调用数据时，由于没有数据可读，且连接已关闭，因此会返回0。
+    if(count == 0){
+        printf("client disconnect: %d\n", fd);
+        close(fd);
+        epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+        return 0;
+    } else if (count < 0){
+        printf("count: %d, errno: %d, %s\n", count, errno, strerror(errno));
+        close(fd);
+        epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+        return 0;
+    }
+
+    conn_list[fd].rlength = count;
+
+#if HTTP_MODE
+    // TODO
+#endif
+
+#if WS_MODE
+    // TODO
+#endif
+
+    // 将监听事件更改为监听可写
+    set_event(fd, EPOLLOUT, 0);
+
+    return count;
+}
+
+int send_cb(int fd){
+#if HTTP_MODE
+    // TODO
+#endif
+
+#if WS_MODE
+    // TODO
+#endif
+
+    int count = 0;
+    if (conn_list[fd].wlength != 0){
+        count = send(fd, conn_list[fd].wbuffer, conn_list[fd].wlength, 0);
+    }
+    set_event(fd, EPOLLIN, 0);
+    return count;
+}
+
+int init_server(unsigned short port){
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in seraddr;
+    seraddr.sin_family = AF_INET;
+    seraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    seraddr.sin_port = htons(port);
+
+    if(-1 == bind(sockfd, (struct sockaddr*)&seraddr, sizeof(struct sockaddr))){
+        printf("bind failed: %s\n", strerror(errno));
+    }
+
+    listen(sockfd, 10);
+    return sockfd;
+}
+
+int main(){
+    insigned short port = 2000;
+
+    epfd = epoll_create(1);
+
+    int i = 0;
+    for(i = 0; i < MAX_PORTS; i++){
+        int sockfd = init_server(port + i);
+
+        conn_list[sockfd].fd = sockfd;
+        conn_list[sockfd].r_action.accept_callback = accept_cb;
+
+        set_event(sockfd, EPOLLIN, 1);
+    }
+
+    // mainloop
+    while(1){
+        struct epoll_event events[1024] = {0};
+        int nready = epoll_wait(epfd, evnets, 1024, -1);
+
+        int i = 0;
+        for(i = 0; i < nready; i++){
+            int connfd = events[i].data.fd;
+
+            if(events[i].events & EPOLLIN){
+                conn_list[connfd].r_action.recv_callback(connfd);
+            }
+            if(evnets[i].events & EPOLLOUT){
+                conn_list[connfd].send_callback(connfd);
+            }
+        }
+    }
+}
---
+++ reactor.h
@@ -0,0 +1,29 @@
+
+#ifndef __REACTOR_H__
+#define __REACTOR_H__
+
+#define BUFFER_LENGTH   1024
+
+typedef int (*RCALLBACK)(int fd); // 前缀R代表Reactor
+
+struct conn {
+    int fd;
+
+    char rbuffer[BUFFER_LENGTH];
+    int rlength;
+
+    char wbuffer[BUFFER_LENGTH];
+    int wlength;
+
+    RCALLBACK send_callback; // 收到可写事件后执行的回调函数
+
+    union 
+    {
+        RCALLBACK recv_callback;
+        RCALLBACK accept_callback;
+    } r_action;
+
+    int status;
+};
+
+#endif
